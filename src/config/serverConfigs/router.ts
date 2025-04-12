321bc1568cd6c4d367198b3b6e63a9dcf4bf52a5
Rafael <rafael.wesley@develops.today>
2025-04-12 06:30:30 -0300

    versao de producao

--- src/config/serverConfigs/router.ts
+++ src/config/serverConfigs/router.ts
@@ -1,7 +1,6 @@
 import ControllerImportarBoleto from "@/lib/controllers/ControllerImportarBoleto.class";
 import ControllerRelatoriosBoletos from "@/lib/controllers/ControllerRelatoriosBoletos.class";
 import ControllerSalvarBoleto from "@/lib/controllers/ControllerSalvarBoletos.class";
-import ServiceBoleto from "@/lib/services/ServiceBoleto.class";
 import { Router } from "express";
 
 const router = Router();
@@ -13,6 +12,6 @@ const controllerRelatoriosBoletos = new ControllerRelatoriosBoletos();
 router.post('/importar-boletos-csv', (req, res) => controllerImportarBoleto.POST(req, res))
 router.get('/importar-boletos-csv', (req, res) => controllerImportarBoleto.GET(req, res))
 router.post('/salvar-boletos-pdf', (req, res) => controllerSalvarBoleto.POST(req, res))
-router.get('/gerar-relatorio', (req, res) => controllerRelatoriosBoletos.GET(req, res))
+router.get('/boletos', (req, res) => controllerRelatoriosBoletos.GET(req, res))
 
 export default router;
